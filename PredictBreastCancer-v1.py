fdc99ad965e3b7a8f74accf330d7d0ce9fbf9fa8
niemtec <janiemiec20@gmail.com>
2018-07-12 15:40:37 +0200

    Added column keys used in the CSV dataset

--- PredictBreastCancer-v1.py
+++ PredictBreastCancer-v1.py
@@ -4,3 +4,41 @@ from sklearn.utils import shuffle
 import matplotlib.gridspec as gridspec
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+# Get the training data file
+train_filename = "data.csv"
+
+# Define column keys (for data.csv)
+idKey = "id"
+diagnosisKey = "diagnosis"
+radiusMeanKey = "radius_mean"
+textureMeanKey = "texture_mean"
+perimeterMeanKey = "perimeter_mean"
+areaMeanKey = "area_mean"
+smoothnessMeanKey = "smoothness_mean"
+compactnessMeanKey = "compactness_mean"
+concavityMeanKey = "concavity_mean"
+concavePointsMeanKey = "concave points_mean"
+symmetryMeanKey = "symmetry_mean"
+fractalDimensionMean = "fractal_dimension_mean"
+radiusSeKey = "radius_se"
+textureSeKey = "texture_se"
+perimeterSeKey = "perimeter_se"
+areaSeKey = "area_se"
+smoothnessSeKey = "smoothness_se"
+compactnessSeKey = "compactness_se"
+concavitySeKey = "concavity_se"
+concavePointsSeKey = "concave points_se"
+symmetrySeKey = "symmetry_se"
+fractalDimensionSeKey = "fractal_dimension_se"
+radiusWorstKey = "radius_worst"
+textureWorstKey = "texture_worst"
+perimeterWorstKey = "perimeter_worst"
+areaWorstKey = "area_worst"
+smoothnessWorstKey = "smoothness_worst"
+compactnessWorstKey = "compactness_worst"
+concavityWorstKey = "concavity_worst"
+concavePointsWorstKey = "concave points_worst"
+symmetryWorstKey = "symmetry_worst"
+fractalDimensionWorstKey = "fractal_dimension_worst"
+
